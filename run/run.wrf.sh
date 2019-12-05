ef2b1c6f65ebba0212dfca0a955c8a8f5950895c
Martin-Tazon <martin.tazon.tdr@gmail.com>
2019-12-05 13:24:34 +0100

    added indications on how to launch

--- run/run.wrf.sh
+++ run/run.wrf.sh
@@ -4,13 +4,13 @@
 #
 #	Runs WRF for all days abailable for the given model (based on days.$run.$exp.lst) for the selected model and experiment (and run)
 #	Special case for HadGEM2-ES
-#		Launch as: nohup run.wrf.sh > nohup.$exp.out 2>&1 &
+#		Launch as: nohup run.wrf.sh > nohup.historical.out 2>&1 &
 #
 ######################################
 
 run=acciona-mx.v3
-mdl=ACCESS1-0
-exp=rcp85
+mdl=MPI-ESM-LR
+exp=historical
 
 scp cloud1.vortex.es:/home/martin/models/head/{model-period.dict,days.$run.$exp.lst} .
 
@@ -31,7 +31,11 @@ for date in `cat days.$run.$exp.lst`;do
 	if [ `echo $date | sed 's/-//g'` -gt ${eyear}1227 ];then break;fi
 	if [ $mdl == HadGEM2-ES ] && [ $exp == historical ] && [ `echo $date | sed 's/-//g'` -gt ${eyear}1127 ];then break;fi
 
+	if [ `echo $date | sed 's/-//g'` -eq 19980602 ];then continue;fi
+	if [ `echo $date | sed 's/-//g'` -eq 20010610 ];then continue;fi
+
 	./wrf.sh $run $mdl $exp $date
+exit
 
 done
 
